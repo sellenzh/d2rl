e91519838ac0286a8122bc3075bf527825487f35
Shilin <sellenzh@gmail.com>
2025-04-26 11:03:00 +0800

    Complete algorithm PETS

--- 3-Cutting_Edge/3-16-2-2-Cross_entropy_method.py
+++ 3-Cutting_Edge/3-16-2-2-Cross_entropy_method.py
@@ -204,3 +204,56 @@ class EnsembleDynamicsModel:
             mean.append(cur_mean.detach().cpu().numpy())
             var.append(cur_var.detach().cpu().numpy())
         return np.hstack(mean), np.hstack(var)
+
+
+
+class FakeEnv:
+    def __init__(self, model):
+        self.model = model
+
+    def step(self, obs, act):
+        inputs = np.concatenate((obs, act), axis=-1)
+        ensemble_model_means, ensemble_model_vars = self.model.predict(inputs)
+        ensemble_model_means[:, :, 1:] += obs.numpy()
+        ensemble_model_stds = np.sqrt(ensemble_model_vars)
+        ensemble_samples = ensemble_model_means + np.random.normal(size=ensemble_model_means.shape) * ensemble_model_stds
+
+        num_models, batch_size, _ = ensemble_model_means.shape
+        models_to_use = np.random.choice([i for i in range(self.model._num_network)], size=batch_size)
+        batch_inds = np.arange(0, batch_size)
+        samples = ensemble_samples[models_to_use, batch_inds]
+        rewards, next_obs = samples[:, :1], samples[:, 1:]
+        return rewards, next_obs
+    
+    def propagate(self, obs, actions):
+        with torch.no_grad():
+            obs = np.copy(obs)
+            total_reward = np.expand_dims(np.zeros(obs.shape[0]), axis=-1)
+            obs, actions = torch.as_tensor(obs), torch.as_tensor(actions)
+            for i in range(actions.shape[1]):
+                action = torch.unsqueeze(actions[:, i], 1)
+                rewards, next_obs = self.step(obs, action)
+                total_reward += rewards
+                obs = torch.as_tensor(next_obs)
+            return total_reward
+        
+    
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def size(self):
+        return len(self.buffer)
+    
+    def return_all_samples(self):
+        all_transitions = list(self.buffer)
+        state, action, reward, next_state, done = zip(*all_transitions)
+        return np.array(state), action, reward, np.array(next_state), done
+        
+
+class PETS:
+    def __init__(self):
+        pass

a881a919b1861925c7c01910d90c5f0c531adc12
Sellen <sellenzh@gmail.com>
2025-06-28 14:36:44 +0800

    create REINFORCE

---
+++ 2-Advanced/9-4-Reinforce.py
@@ -0,0 +1,116 @@
+from gettext import translation
+import gym
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import rl_utils
+
+
+class PolicyNet(nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim) -> None:
+        super(PolicyNet, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim=1)
+
+
+class REINFORCE:
+    def __init__(self, state_dim, hidden_dim, action_dim, learning_rate, gamma, device) -> None:
+        self.policy_net = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=learning_rate)
+        self.gamma = gamma
+        self.device = device
+
+    def take_action(self, state: np.ndarray) -> int:
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        probs = self.policy_net(state)
+        action_list = torch.distributions.Categorical(probs)
+        action = action_list.sample()
+        return action.item()
+
+    def update(self, transition_dict: dict) -> None:
+        reward_list = transition_dict['rewards']
+        state_list = transition_dict['states']
+        action_list = transition_dict['actions']
+        G = 0
+        self.optimizer.zero_grad()
+        for i in reversed(range(len(reward_list))):
+            reward = reward_list[i]
+            state = torch.tensor([state_list[i]], dtype=torch.float).to(self.device)
+            action = torch.tensor([action_list[i]]).view(-1, 1).to(self.device)
+            log_prob = torch.log(self.policy_net(state).gather(1, action))
+            G = self.gamma * G + reward
+            loss = -log_prob * G
+            loss.backward()
+        self.optimizer.step()
+
+
+learning_rate = 1e-3
+num_episodes = 1000
+hidden_dim = 128
+gamma = 0.98
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
+    "cpu")
+
+env_name = "CartPole-v1"
+env = gym.make(env_name)
+torch.manual_seed(0)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+agent = REINFORCE(state_dim, hidden_dim, action_dim, learning_rate, gamma,
+                  device)
+
+return_list = []
+for i in range(10):
+    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+        for i_episode in range(int(num_episodes / 10)):
+            episode_return = 0
+            transition_dict = {
+                'states': [],
+                'actions': [],
+                'next_states': [],
+                'rewards': [],
+                'dones': []
+            }
+            state = env.reset()[0]
+            done = False
+            while not done:
+                action = agent.take_action(state)
+                next_state, reward, done, _, _ = env.step(action)
+                transition_dict['states'].append(state)
+                transition_dict['actions'].append(action)
+                transition_dict['next_states'].append(next_state)
+                transition_dict['rewards'].append(reward)
+                transition_dict['dones'].append(done)
+                state = next_state
+                episode_return += reward
+            return_list.append(episode_return)
+            agent.update(transition_dict)
+            if (i_episode + 1) % 10 == 0:
+                pbar.set_postfix({
+                    'episode':
+                    '%d' % (num_episodes / 10 * i + i_episode + 1),
+                    'return':
+                    '%.3f' % np.mean(return_list[-10:])
+                })
+            pbar.update(1)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('REINFORCE on {}'.format(env_name))
+plt.savefig('REINFORCE-1.jpg')
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('REINFORCE on {}'.format(env_name))
+plt.savefig('REINFORCE-2.jpg')
